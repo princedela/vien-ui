5f7ad322ea6a7ac07b28c5c7c6b761ef7b2e3992
dela <dev.delaprince@gmail.com>
2022-05-04 13:54:44 +0000

    Initial Commit: Created Home Page Layout

---
+++ vien-frontend/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
